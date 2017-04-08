e5a4b21d10875d0290efc1638561c2213fd29b30
Mr.Pain <ramil.talipov@gmail.com>
2017-04-08 22:52:13 +0300

    version 2

--- 8ball.rb
+++ 8ball.rb
@@ -1,13 +1,18 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
 
 request = [
-
   "Привет, дорогой друг. Отвечаю на твой вопрос...",
   "Кто вопрошает, тот получит ответ:",
   "Здравствуй, смертный. Сегодня для тебя такой ответ:"
 ]
-
 puts request.sample
-
 sleep 5
 
 answers = [
